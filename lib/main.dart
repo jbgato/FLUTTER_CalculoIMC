cd3d7a0023c2588f2e72cc7a3b4d8a597b7749f7
jbgato <joicegato@gmail.com>
2020-08-29 12:49:59 -0300

    Updates

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main(){
+void main() {
   runApp(MaterialApp(
-    home: Home() ,
+    home: Home(),
   ));
 }
+
 class Home extends StatefulWidget {
   @override
   _HomeState createState() => _HomeState();
@@ -17,6 +19,48 @@ class _HomeState extends State<Home> {
       appBar: AppBar(
         title: Text("Calculadora de IMC"),
         centerTitle: true,
+        backgroundColor: Colors.green,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.refresh),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      backgroundColor: Colors.white,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment
+            .stretch, // Alinhar a coluna de forma horizontal centro -  Stretch ocupando toda a largura.
+        children: <Widget>[
+          Icon(Icons.person_outline, color: Colors.green, size: 80.00), //icones
+          TextField(
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(
+              labelText: "Peso(kg):",
+              labelStyle: TextStyle(color: Colors.green),
+            ),
+            textAlign: TextAlign.center,
+            style: TextStyle(color: Colors.green, fontSize: 25.0),
+          ), // Campo de texto.
+          TextField(
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(
+              labelText: "Altura(cm):",
+              labelStyle: TextStyle(color: Colors.green),
+            ),
+            textAlign: TextAlign.center,
+            style: TextStyle(color: Colors.green, fontSize: 25.0),
+          ),
+          Container(
+            height: 50.0,
+            child: RaisedButton(
+              onPressed: () {},
+              child: Text("Calcular",
+                  style: TextStyle(color: Colors.white, fontSize: 25.0)),
+              color: Colors.green,
+            ), //botao com fundo colorido.
+          ),
+        ],
       ),
     ); //Widget para barras e botões
   }
